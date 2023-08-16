629146a9f1f0aecd090403fab65e90f1c22e960f
mechiko <mechiko@users.noreply.github.com>
2023-08-16 23:55:59 +0300

    Update dialog.go 2

--- declarative/dialog.go
+++ declarative/dialog.go
@@ -56,6 +56,7 @@ type Dialog struct {
 	CancelButton  **walk.PushButton
 	DefaultButton **walk.PushButton
 	FixedSize     bool
+	Borderless    bool
 }
 
 func (d Dialog) Create(owner walk.Form) error {
